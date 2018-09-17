95feaf6d01fd632b4a465ddab10941af2fdfb96b
Devin Eldreth <devin.eldreth@gmail.com>
2018-09-16 21:43:53 -0400

    Add prettier, pretty-quick, and husky.

--- src/templates/blog-tags.js
+++ src/templates/blog-tags.js
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link, graphql } from 'gatsby';
+import { ThemeProvider } from 'emotion-theming';
+import styled from 'react-emotion';
+import Header from '../components/header';
 
-const Tags = ({ pathContext, data }) => {
-  const { tag } = pathContext;
+import Grid from '../components/layouts/grid';
+import { rhythm } from '../utils/typography';
+import theme from '../utils/theme';
+import Post from '../components/post';
+
+const Tags = ({ pageContext, data }) => {
+  const { tag } = pageContext;
   const { edges, totalCount } = data.allMarkdownRemark;
   const tagHeader = `${totalCount} post${
     totalCount === 1 ? "" : "s"
   } tagged with "${tag}"`;
 
   return (
-    <div>
-      <h1>{tagHeader}</h1>
-      <ul>
-        {edges.map(({ node }) => {
-          const { path, title } = node.frontmatter;
-          console.log(node.frontmatter);
-          return (
-            <li key={path}>
-              <Link to={path}>{title}</Link>
-            </li>
-          );
-        })}
-      </ul>
-      {/*
-              This links to a page that does not yet exist.
-              We'll come back to it!
-            */}
-      <Link to="/tags">All tags</Link>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <Header/>
+
+        <TagHeader>{tagHeader}</TagHeader>
+
+        <Grid>
+          {edges.map(({ node }) => {
+            return <Post key={node.fields.slug} post={node}/>
+          })}
+        </Grid>
+      </Fragment>
+    </ThemeProvider>
   );
 };
+
+const TagHeader = styled('h1')`
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
+  margin-top: ${rhythm(2)};
+`;
 
 export default Tags;
 
@@ -43,8 +52,14 @@ export const pageQuery = graphql`
       totalCount
       edges {
         node {
+          excerpt
+          fields {
+            slug
+          }
           frontmatter {
+            date(formatString: "MMMM D YYYY")
             title
+            tags
           }
         }
       }
