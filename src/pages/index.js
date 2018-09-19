60e11c87bc72d5bdec65b5fe90eeeb1494dc180f
Devin Eldreth <devin.eldreth@gmail.com>
2018-09-19 12:50:47 -0400

    Filter from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,10 @@ export const pageQuery = graphql`
         description
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { ne: false } } }
+    ) {
       edges {
         node {
           excerpt
