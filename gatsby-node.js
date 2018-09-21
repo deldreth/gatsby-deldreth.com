37f4276296e8401371e94ff5ffad5c3fb0a7eb7f
Devin Eldreth <devin.eldreth@gmail.com>
2018-09-21 09:59:48 -0400

    Fix failing build.

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,6 +17,7 @@ exports.createPages = ({ graphql, actions }) => {
             allMarkdownRemark(
               limit: 1000
               sort: { fields: [frontmatter___date], order: DESC }
+              filter: { frontmatter: { published: { ne: false } } }
             ) {
               edges {
                 node {
